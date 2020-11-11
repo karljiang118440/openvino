2a5bd487efdf323ead61e4a84dca507c64438518
karljiang118440 <32283913+karljiang118440@users.noreply.github.com>
2020-11-11 15:48:28 +0800

    run.sh

---
+++ scripts/run.sh
@@ -0,0 +1,13 @@
+./hello_classification /media/jcq/Soft/OpenVino/open_model_zoo/tools/accuracy_checker/data/test_models/SampLeNet.xml /media/jcq/Soft/OpenVino/intel/openvino_2020.4.287/deployment_tools/demo/car_1.bmp CPU
+
+./hello_classification /media/jcq/Soft/OpenVino/open_model_zoo/tools/accuracy_checker/data/test_models/SampLeNet.xml /media/jcq/Soft/OpenVino/intel/openvino_2020.4.287/deployment_tools/demo/car.png CPU
+
+
+
+
+
+
+
+./multi_channel_human_pose_estimation_demo -i /media/jcq/Soft/Opencv/dms_data/source_video/call.mp4 -m /media/jcq/Soft/OpenVino/intel/openvino_2020.4.287/deployment_tools/open_model_zoo/tools/downloader/intel/human-pose-estimation-0001/FP32/human-pose-estimation-0001.xml -d CPU
+
+./multi_channel_face_detection_demo -i /media/jcq/Soft/Opencv/dms_data/source_video/call.mp4 -m /media/jcq/Soft/OpenVino/intel/openvino_2020.4.287/deployment_tools/open_model_zoo/tools/downloader/intel/face-detection-0100/FP32/face-detection-0100.xml -r CPU
